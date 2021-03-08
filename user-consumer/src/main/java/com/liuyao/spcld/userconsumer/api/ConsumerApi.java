b2ba091f80c443658fea6137fd1d71c190bfc1e4
liuyao <liuyaocoool@163.com>
2021-03-08 08:43:00 +0800

    arrange module port

--- user-consumer/src/main/java/com/liuyao/spcld/userconsumer/api/ConsumerApi.java
+++ user-consumer/src/main/java/com/liuyao/spcld/userconsumer/api/ConsumerApi.java
@@ -3,6 +3,9 @@ package com.liuyao.spcld.userconsumer.api;
 import com.liuyao.spcld.userapi.api.UserApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
+/**
+ * 会从eureka中做负载
+ */
 @FeignClient(name = "user-provider")
 public interface ConsumerApi extends UserApi {
 
--- user-provider/src/main/java/com/liuyao/spcld/userprovider/controller/UserApiController.java
+++ user-provider/src/main/java/com/liuyao/spcld/userprovider/controller/UserApiController.java
@@ -1,16 +1,19 @@
 package com.liuyao.spcld.userprovider.controller;
 
 import com.liuyao.spcld.userapi.api.UserApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController implements UserApi {
 
+    @Value("${server.port}")
+    String port;
 
     @Override
     public String alive(){
-        return "ok";
+        return port + " ok";
     }
 
 }
