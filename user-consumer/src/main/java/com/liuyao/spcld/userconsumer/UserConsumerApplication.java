edb01f9ae2f6f0a34a69f49bb88cceeef151aca9
liuyao <liuyaocoool@163.com>
2021-03-09 11:51:56 +0800

    信号量隔离 线程池隔离

--- user-consumer/src/main/java/com/liuyao/spcld/userconsumer/UserConsumerApplication.java
+++ user-consumer/src/main/java/com/liuyao/spcld/userconsumer/UserConsumerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableCircuitBreaker // 启动Hystrix的注解
 @EnableFeignClients //内置Hystrix的注解 所以feign整合hystrix不需要加上一个注解
+@EnableHystrixDashboard
 public class UserConsumerApplication {
 
     public static void main(String[] args) {
