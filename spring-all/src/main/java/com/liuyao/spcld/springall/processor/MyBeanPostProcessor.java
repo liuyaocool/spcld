7b0493b328aaec019023410900a38037688f49cd
刘耀 <zl_liuy@163.com>
2021-04-12 19:22:21 +0800

    spring ioc autowire spel xml

--- spring-all/src/main/java/com/liuyao/spcld/springall/processor/MyBeanPostProcessor.java
+++ spring-all/src/main/java/com/liuyao/spcld/springall/processor/MyBeanPostProcessor.java
@@ -23,7 +23,7 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
             // ...
         }
 
-        System.out.println("before init: " + beanName);
+//        System.out.println("before init: " + beanName);
         return bean;
     }
 
@@ -37,7 +37,7 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
      */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("after init: " + beanName);
+//        System.out.println("after init: " + beanName);
         return bean;
     }
 }
--- spring-all/src/test/java/com/liuyao/spcld/springall/IOCTest.java
+++ spring-all/src/test/java/com/liuyao/spcld/springall/IOCTest.java
@@ -1,8 +1,11 @@
 package com.liuyao.spcld.springall;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.liuyao.spcld.springall.eneity.Person;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.SQLException;
 
 public class IOCTest {
 
@@ -11,7 +14,7 @@ public class IOCTest {
     final ClassPathXmlApplicationContext ctx2 = new ClassPathXmlApplicationContext("ioc-depends.xml");
 
     @Test
-    public void test01() {
+    public void test01() throws SQLException {
 //        getBean("person");
 //        getBean("person2");
 //        getBean("person3");
@@ -25,7 +28,16 @@ public class IOCTest {
 //        getBean("personInstanceFac");
 
 //        getBean("myFactoryBean");
-        getBean("person7");
+//        getBean("person7");
+
+//        final DruidDataSource dataSource = getBean("dataSource", DruidDataSource.class);
+//        System.out.println(dataSource.getConnection());
+
+//        final DruidDataSource dataSourceProperties = getBean("dataSourceProperties", DruidDataSource.class);
+//        System.out.println(dataSourceProperties.getConnection());
+
+//        getBean("person8");
+        getBean("person9");
 
 //        ctx.close();
 //        getBean2("person");
@@ -34,17 +46,19 @@ public class IOCTest {
     }
 
     private Person getBean(String name) {
-        final Person person = ctx.getBean(name, Person.class);
-        System.out.println(name + ": " + person);
-        return person;
+        return getBean(ctx, name, Person.class);
+    }
+
+    private <T> T getBean(String name, Class<T> tClass) {
+        return getBean(ctx, name, tClass);
     }
 
     private Person getBean2(String name) {
-        return getBean2(name, Person.class);
+        return getBean(ctx2, name, Person.class);
     }
 
-    private <T> T getBean2(String name, Class<T> clas) {
-        final T person = ctx2.getBean(name, clas);
+    private <T> T getBean(ClassPathXmlApplicationContext appCtx, String name, Class<T> clas) {
+        final T person = appCtx.getBean(name, clas);
         System.out.println(name + ": " + person);
         return person;
     }
