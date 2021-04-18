2e792d31d5d20a5adcac22ac6639ca11eefbd4ba
刘耀 <zl_liuy@163.com>
2021-04-18 15:55:34 +0800

    apollo

---
+++ coonfig-client-apollo/src/main/java/com/liuyao/spcld/coonfigclientapollo/CoonfigClientApolloApplication.java
@@ -0,0 +1,13 @@
+package com.liuyao.spcld.coonfigclientapollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoonfigClientApolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoonfigClientApolloApplication.class, args);
+    }
+
+}
---
+++ coonfig-client-apollo/src/test/java/com/liuyao/spcld/coonfigclientapollo/CoonfigClientApolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuyao.spcld.coonfigclientapollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoonfigClientApolloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
